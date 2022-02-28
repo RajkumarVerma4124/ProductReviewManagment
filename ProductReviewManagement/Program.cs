08c842084004edfa653f03f1c1662b13527d4ca7
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-28 17:14:47 +0530

    [Raj] Added:- The welcome message in the main program

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        //Entry of the main program
+        public static void Main(string[] args)
+        {
+            //Displaying welcome message
+            Console.WriteLine("Welcome To The Product Review Management Program");
+            Console.ReadLine();
+        }
+    }
+}
